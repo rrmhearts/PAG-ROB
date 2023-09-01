614107d33238bcccc3cb21812b001cd3ca3ce220
Ryan <rrmhearts@gmail.com>
2023-09-01 16:13:54 -0400

    working saliency with captum.. only 1 image at a time!

--- test_CIFAR10.py
+++ test_CIFAR10.py
@@ -11,7 +11,22 @@ import numpy as np
 import random
 import yaml
 import os
+import matplotlib.pyplot as plt
+import numpy as np
 
+import gc
+# import torch
+# import torchvision
+# import torchvision.transforms as transforms
+import torchvision.transforms.functional as TF
+
+from torchvision import models
+
+from captum.attr import IntegratedGradients
+from captum.attr import Saliency
+from captum.attr import DeepLift
+from captum.attr import NoiseTunnel
+from captum.attr import visualization as viz
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Do PAG Imply Robustness?')
     parser.add_argument('--config_path', type=str, default='./configs/cifar10_sbg_rn18.yaml', help='training config path')
@@ -27,7 +42,7 @@ if __name__ == "__main__":
     # Reproducibility
     random.seed(config["seed"])
     torch.manual_seed(config["seed"])
-    torch.cuda.manual_seed_all(config["seed"])
+    # torch.cuda.manual_seed_all(config["seed"])
     np.random.seed(config["seed"])
 
 
@@ -47,8 +62,9 @@ if __name__ == "__main__":
     transform = torchvision.transforms.Compose(
         [torchvision.transforms.ToTensor()])
 
+    batch_size = 1
     testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=0)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=0)
 
     # get model
     # if config["arch"] == 'vit':
@@ -56,25 +72,113 @@ if __name__ == "__main__":
     # else:
     model = torch.nn.DataParallel(ResNet18()).cuda()
     # print architecture details
-    print(f'Using {config["arch"]} with {sum(p.numel() for p in model.parameters() if p.requires_grad)} learnable params')
+    # print(f'Using {config["arch"]} with {sum(p.numel() for p in model.parameters() if p.requires_grad)} learnable params')
 
     # get optimizer
-    optimizer = torch.optim.SGD(model.parameters(), lr=config["lr"], momentum=config["momentum"],
-                                weight_decay=config["weight_decay"])
+    # optimizer = torch.optim.SGD(model.parameters(), lr=config["lr"], momentum=config["momentum"],
+    #                             weight_decay=config["weight_decay"])
 
     model.load_state_dict(torch.load("models/weights/rn18-c10-SBG.pt"))
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    c_acc, r_acc = eval_adv_test_whitebox(model=model, device=device, test_loader=testloader, epsilon=0.5, num_steps=7,
-                                            step_size=1.5 * 0.5 / 7, random=True, verbose=False, norm='l_2',
-                                            stats=data_stats)
-    if config["use_wandb"]:
-        wandb.log({"L2: c_acc": c_acc, "r_acc": r_acc})
-    print(f"L2: c_acc: {c_acc} r_acc: {r_acc}")
+    # c_acc, r_acc = eval_adv_test_whitebox(model=model, device=device, test_loader=testloader, epsilon=0.5, num_steps=7,
+    #                                         step_size=1.5 * 0.5 / 7, random=True, verbose=False, norm='l_2',
+    #                                         stats=data_stats)
+    # if config["use_wandb"]:
+    #     wandb.log({"L2: c_acc": c_acc, "r_acc": r_acc})
+    # print(f"L2: c_acc: {c_acc} r_acc: {r_acc}")
 
-    c_acc, r_acc = eval_adv_test_whitebox(model=model, device=device, test_loader=testloader, epsilon=0.5, num_steps=7,
-                                            step_size=1.5 * 0.5 / 7, random=True, verbose=False, norm='l_inf',
-                                            stats=data_stats)
-    if config["use_wandb"]:
-        wandb.log({"Linf: c_acc": c_acc, "r_acc": r_acc})
-    print(f"Linf: c_acc: {c_acc} r_acc: {r_acc}")
+    # c_acc, r_acc = eval_adv_test_whitebox(model=model, device=device, test_loader=testloader, epsilon=0.5, num_steps=7,
+    #                                         step_size=1.5 * 0.5 / 7, random=True, verbose=False, norm='l_inf',
+    #                                         stats=data_stats)
+    # if config["use_wandb"]:
+    #     wandb.log({"Linf: c_acc": c_acc, "r_acc": r_acc})
+    # print(f"Linf: c_acc: {c_acc} r_acc: {r_acc}")
+
+
+    classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    def imshow(img, transpose = True):
+        img = img / 2 + 0.5     # unnormalize
+        npimg = img.numpy()
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+        plt.show()
+
+    dataiter = iter(testloader)
+    images, labels = next(dataiter)
+
+    # print images
+    imshow(torchvision.utils.make_grid(images))
+    print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
+
+
+    outputs = model(images.cuda())
+
+    _, predicted = torch.max(outputs, 1)
+
+    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                                for j in range(batch_size)))
+    
+    # Choose an image for captum
+    ind = 0
+    input = images[ind].unsqueeze(0).cuda()
+    input.requires_grad = True
+    model.eval()
+
+    # testloader = None
+    # testset = None
+    # gc.collect() # Python thing
+    # torch.cuda.empty_cache() # PyTorch thing
+
+
+    def attribute_image_features(algorithm, input, **kwargs):
+        model.zero_grad()
+        tensor_attributions = algorithm.attribute(input,
+                                                target=labels[ind],
+                                                **kwargs
+                                                )
+        
+        return tensor_attributions
+    
+    saliency = Saliency(model)
+    grads = saliency.attribute(input, target=labels[ind].item())
+    grads = np.transpose(grads.squeeze().cpu().detach().numpy(), (1, 2, 0))
+
+
+    # ig = IntegratedGradients(model)
+    # attr_ig, delta = attribute_image_features(ig, input, baselines=input * 0, return_convergence_delta=True)
+    # attr_ig = np.transpose(attr_ig.squeeze().cpu().detach().numpy(), (1, 2, 0))
+    # print('Approximation delta: ', abs(delta))
+
+    # ig = IntegratedGradients(model)
+    # nt = NoiseTunnel(ig)
+    # attr_ig_nt = attribute_image_features(nt, input, baselines=input * 0, nt_type='smoothgrad_sq',
+    #                                     nt_samples=100, stdevs=0.2)
+    # attr_ig_nt = np.transpose(attr_ig_nt.squeeze(0).cpu().detach().numpy(), (1, 2, 0))
+
+    # dl = DeepLift(model)
+    # attr_dl = attribute_image_features(dl, input, baselines=input * 0)
+    # attr_dl = np.transpose(attr_dl.squeeze(0).cpu().detach().numpy(), (1, 2, 0))
+
+    print('Original Image')
+    print('Predicted:', classes[predicted[ind]], 
+        ' Probability:', torch.max(F.softmax(outputs, 1)).item())
+
+    original_image = np.transpose((images[ind].cpu().detach().numpy() / 2) + 0.5, (1, 2, 0))
+
+    _ = viz.visualize_image_attr(None, original_image, 
+                        method="original_image", title="Original Image")
+
+    _ = viz.visualize_image_attr(grads, original_image, method="blended_heat_map", sign="absolute_value",
+                            show_colorbar=True, title="Overlayed Gradient Magnitudes")
+
+    # _ = viz.visualize_image_attr(attr_ig, original_image, method="blended_heat_map",sign="all",
+    #                         show_colorbar=True, title="Overlayed Integrated Gradients")
+
+    # _ = viz.visualize_image_attr(attr_ig_nt, original_image, method="blended_heat_map", sign="absolute_value", 
+    #                             outlier_perc=10, show_colorbar=True, 
+    #                             title="Overlayed Integrated Gradients \n with SmoothGrad Squared")
+
+    # _ = viz.visualize_image_attr(attr_dl, original_image, method="blended_heat_map",sign="all",show_colorbar=True, 
+    #                         title="Overlayed DeepLift")
